cccf9c8865e076e4942ff2b4a019306caf525c64
ajivettickal <ajivettickal@gmail.com>
2025-06-22 20:57:18 +0530

    'nine'

--- Amora_Costa/settings.py
+++ Amora_Costa/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^1_29&3_)&et7@id(7tljotzgg^de4x)1sv)itntl+ig5=+ai='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'True'
+DEBUG = 'False'
 
 ALLOWED_HOSTS = ["amora-costa.up.railway.app",'localhost',]
 
