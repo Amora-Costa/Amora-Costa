ab2c2ba3f0b5ec6e4c18f02abd9dceac1b14454a
ajivettickal <ajivettickal@gmail.com>
2025-06-23 15:21:18 +0530

    'one'

--- Amora_Costa/urls.py
+++ Amora_Costa/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import Templateview
+from django.views.generic import TemplateView
 urlpatterns = [
     path('',include('Amora.urls')),
     path('admin/', admin.site.urls),
