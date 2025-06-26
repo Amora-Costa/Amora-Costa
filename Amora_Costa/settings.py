83e4ff04a69a2267f2910cc1ff502ea55d0686b4
ajivettickal <ajivettickal@gmail.com>
2025-06-26 10:44:09 +0530

    with logo

--- Amora_Costa/settings.py
+++ Amora_Costa/settings.py
@@ -43,7 +43,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS',default ='127.0.01')
 
 
 # Application definition
